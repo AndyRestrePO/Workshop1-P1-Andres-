a87d4e55e7dd3223283f56e6a6104d9a3e1335c9
afrestrepg <andres.restrepog05@gmail.com>
2026-08-20 10:22:13 -0500

    llenado de la base de datos de peliculas

--- movie/models.py
+++ movie/models.py
@@ -6,6 +6,8 @@ class Movie(models.Model):
     image = models.ImageField(upload_to='movie/images/')
     description = models.TextField()
     url = models.URLField(blank=True, null=True)
+    genre = models.CharField(max_length=200, blank=True)
+    year = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.title
